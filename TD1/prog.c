a50dca0cbdb17991ead871b548503524abd7fe63
Lear-kao <axelaudeoud66@gmail.com>
2025-01-23 15:32:08 +0100

    Update prog.c

--- TD1/prog.c
+++ TD1/prog.c
@@ -2,7 +2,7 @@
 #define MAX_NOEUDS 20
 
 int papa[MAX_NOEUDS], feuille[MAX_NOEUDS];
-int n, n_feuille;//n <= MAX_NOEUDS
+int n, n_feuille = 0;//n <= MAX_NOEUDS
 
 //saisie d'un arbre par tableau de papa
 
@@ -12,5 +12,29 @@ void saisie( int n )
     {
         printf("entrer papa noeud %d stp",i);
         scanf("%d",&(papa[i]));
+    }
+}
+
+void feuille_arbre(int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        /* tester si i est une feuille */
+        int est_feuille = 1;
+        int j = 0;
+        while( est_feuille != 0 && j < n)
+        {
+            if( papa[j] == i )
+            {
+                est_feuille = 0;
+            }
+            j++;
+        }
+        if ( est_feuille == 1 )
+        {
+            feuille[n_feuille] = i;
+            n_feuille++;
+        }
+        est_feuille = 1;
     }
 }
