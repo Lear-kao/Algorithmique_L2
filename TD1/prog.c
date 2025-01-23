bde417977575903d5fcbcf0bed69f0d170aae0ef
Lear-kao <axelaudeoud66@gmail.com>
2025-01-23 16:06:43 +0100

    Update prog.c

--- TD1/prog.c
+++ TD1/prog.c
@@ -38,3 +38,42 @@ void feuille_arbre(int n)
         est_feuille = 1;
     }
 }
+
+int monter_arbre( int f )
+{
+    // f est  une feuille. monter de f jusqu'à racine
+
+    int h = 0;
+    while( papa[f] != -1)
+    {
+        h ++;
+        f = papa[f]
+    }
+    return h;
+}
+
+int hauteur_arbre( void )
+{
+    int max = 0;
+    for  (int i = 0; i < n_feuille ; i++)
+    {
+        int temp = monter_arbre( feuille[i] )
+        if  ( temp > max )
+        {
+            max = temp;
+        }
+    }
+    return max;
+}
+
+
+int main( void )
+{
+    printf("rentrer le nombre de noeuds \n");
+    scanf("%d",&n);
+    saisie(n);
+    n_feuille = feuille_arbre(n);
+    int d = hauteur_arbre();
+    printf("%d",d);
+    return  0;
+}
