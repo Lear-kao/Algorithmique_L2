5764b13535a99132c7b937d207552b6edef18fe2
Lear-kao <axelaudeoud66@gmail.com>
2025-04-01 18:52:11 +0200

    ytr

--- prog_gen/algo_calc.c
+++ prog_gen/algo_calc.c
@@ -18,3 +18,52 @@ Prend en  entrée un tableau de charactère et renvoie un arbre binaire d'opéra
     return p;
 }
 
+/* 
+Def:  Invariance de la boucle;
+    Assertion, propriétée ou relation entre des variables d'un programme 
+    toujours vraie à un point précis dans  une boucle (souvent à l'entrée de la boucle) 
+    quel que soit le nombre d'éxecution effectué par la boucle.
+
+Cette notion est utilisée pour la preuve  d'un algo ou d'un programmme.
+*/
+
+/* 
+Ex_Trouver l'invariant de la boucle :
+    pgcd: Algo Euclide
+pgcd(m,n):  n si m%n == 0;
+            pgcd(n,m%n) si m%n != 0;
+*/
+
+int pgcd(int m,int n)
+{
+    if (m%n == 0) return  n;
+    return pgcd(n,m%n);
+}
+
+int pgcd_ite(int m,int n)
+{
+    if (m%n == 0) return  n;
+    return pgcd(n,m%n);
+}
+
+int pgcd_ite(int m,int n)
+/* invariant de la boucle : pgcd(m,n) = pgcd(n,r) */
+{
+    int r = m%n;
+    while( r!= 0)
+    {
+        m = n;
+        n = r;
+        r = m%n;
+    }
+    return r;
+}
+
+/* 
+Bonus: 
+    Etendre le programme  d'évaluation d'une expression préfixe pour les opérateurs
+    +,-,*,/,%, ^(exponentiel) et des variables (ex: abc)
+EX: (4*2)+5/3
+    + * 4 2 / 5 3
+
+*/
